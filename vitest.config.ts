2f82b03d49c29b49693be0dd3e7bda9e8fb94f77
Beto Frega <637874+BetoFrega@users.noreply.github.com>
2025-09-07 13:54:57 +0000

    feat: add vite-tsconfig-paths plugin and update coverage provider to v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vitest/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsConfigPaths()],
   test: {
     globals: true,
     environment: "node",
     coverage: {
+      provider: "v8",
       reporter: ["text", "json", "html"],
       exclude: ["node_modules/", "dist/"],
     },
